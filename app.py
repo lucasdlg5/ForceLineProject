285eaddfb865733138d8243dbf0a4ad3f4a69714
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-03-11 21:13:30 -0300

    Melhorei a mensagem do App.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "42 é a resposta do universo!"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
