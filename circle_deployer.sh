a98156b2f92d7db661da5158cf663707b264212a
Gustavo <gustavo.silva6785@gmail.com>
2019-04-06 22:22:14 -0300

    refazendo arquivo de config do travis

--- circle_deployer.sh
+++ circle_deployer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 git pull
+sudo docker login -u "forcelinerobot" -p "789456qwe"
 sudo docker build -t forcelineproject:latest .
 sudo docker run -d --name forceline --restart=always -p 8000:8000 forcelineproject:latest
