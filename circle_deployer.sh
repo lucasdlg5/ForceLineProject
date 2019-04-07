bbd4d7307366227924258dbbf7939d4766d0cdf5
Gustavo <gustavo.silva6785@gmail.com>
2019-04-06 22:43:41 -0300

    refatorando arquivo sh

--- circle_deployer.sh
+++ circle_deployer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+sudo docker forceline
+sudo rm forceline
 git pull
 sudo docker login -u "forcelinerobot" -p "789456qwe"
 sudo docker build -t forcelineproject:latest .
-sudo docker run -d --restart=always -p 8000:8000 forcelineproject:latest
+sudo docker run -d --name forceline --restart=always -p 8000:8000 forcelineproject:latest
