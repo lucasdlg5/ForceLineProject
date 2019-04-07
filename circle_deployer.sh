6cb89e8003dc2e9b1d07286dd948442cc0f22d50
Gustavo <gustavo.silva6785@gmail.com>
2019-04-06 21:10:41 -0300

    config do travis

--- circle_deployer.sh
+++ circle_deployer.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 git pull
-docker build -t forceline:latest .
-docker run -d --name forceline --restart=always -p 8000:8000 forceline:latest
+docker run -d --name forceline --restart=always -p 8000:8000 forcelineproject:latest
