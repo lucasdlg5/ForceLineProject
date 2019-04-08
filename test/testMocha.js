06587ecc7b174b8ed56010cfe41e38d0451dd4ef
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-08 15:24:11 -0300

    Alterando testes

--- test/testMocha.js
+++ test/testMocha.js
@@ -1,56 +1,26 @@
 var assert = require('assert');
 
-// before(function(done) {
-// 	//run asynchronous setup
-    
-//     //tell mocha when you are done
-//     done();
-// });
+var phantom = require('phantom');
 
-var webPage = require("webpage").create();
-var page = webPage.create();
-var args = require('system').args;
+describe('Mocha and phantom', function () {
 
-//pass in the name of the file that contains your tests
- var testFile = '.\testMain.js';
-//pass in the url you are testing
-var pageAddress = "http://206.81.8.182:8000";
+    this.timeout(150);
 
-// if (typeof testFile === 'undefined') {
-//     console.error("Did not specify a test file");
-//     phantom.exit();
-// }
-
-page.open(pageAddress, function(status) {
-  if (status !== 'success') {
-      console.error("Failed to open", page.frameUrl);
-      phantom.exit();
-  }
+    it('Tweeking with phantomjs', function (done) {
+        phantom.create(function (ph) {
+            ph.createPage(function (page) {
+                page.open('http://206.81.8.182:8000', function (status) {
 
 
-  if (window.callPhantom) {
-    window.callPhantom({ message: args.join(" ") });
-} else {
-    console.log( args.join(" ") );
-}
-
-
-  //Inject mocha and chai     						  page.injectJs("../node_modules/mocha/mocha.js");
-  page.injectJs("../node_modules/chai/chai.js");
-
-	//inject your test reporter
-    page.injectJs("mocha/reporter.js");
-
-	//inject your tests
-    page.injectJs("mocha/" + testFile);
- 
-    page.evaluate(function() {
-        window.mocha.run();
-    });
-});
-
-page.onCallback = function(data) {
-  data.message && console.log(data.message);
-  data.exit && phantom.exit();
-};
-
+                    page.evaluate(function () {
+                       return document.all[0].outerHTML      //can check different elements
+                    }, function (result) {
+                        console.log('----------->>>>result',result);
+                        assert.equal(status,'success','Not appropriate status');
+                        done();
+                    })
+                })
+            })
+        })
+    })
+})
