ac669fb598067267d3756afe97aa94f3d1315a83
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-08 14:56:55 -0300

    Adicionando Teste de teste

--- test/testMocha.js
+++ test/testMocha.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 describe('Pagina Principal', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal("42 é a resposta do universo!!!!!!", document.getElementById("resposta").innerHTML);
     });
 });
