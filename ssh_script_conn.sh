1387b21f93483d3b0da3a23ed40ca3845d57eb4d
Gustavo <gustavo.silva6785@gmail.com>
2019-05-20 22:37:42 -0300

    fix travis

--- ssh_script_conn.sh
+++ ssh_script_conn.sh
@@ -6,3 +6,11 @@ port="22"
 host="206.81.8.182"
 
 sshpass -p $psswd ssh $user@$host -p $port
+#!/bin/bash
+
+user="root"
+psswd="ynk543yo10"
+port="22"
+host="206.81.8.182"
+
+sshpass -p $psswd ssh $user@$host -p $port
