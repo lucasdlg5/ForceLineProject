c73f86300c01fa29e90b6a7ff04e75f0f224ac5a
mayaracsferreira <mayara_csf@hotmail.com>
2019-05-28 08:06:23 -0300

    página home

---
+++ todoapp/static/home.1.js
@@ -0,0 +1,78 @@
+const add_task = document.getElementById('add-task')
+const dataContainer = document.getElementById('data')
+
+window.addEventListener("load", () => {
+  const sessionKey = window.sessionStorage.getItem('key')
+
+  if (!sessionKey) {
+    window.location.href = "http://localhost:8000/"
+  }
+
+  const id = getUserId()
+
+  getAllTasksAndRenderData(id)
+})
+
+getUserId = () => {
+  let session = window.sessionStorage.getItem('key')
+
+  return session.substr(session.indexOf('?') + 1, session.length - 1)
+}
+
+const getAllTasksAndRenderData = (id) => {
+  data = []
+
+  axios.get('http://localhost:8000/task/getAll/' + id + '')
+    .then(function (response) {
+      response.data[1].forEach((i) => {
+
+        data.push(
+          "<ul><li>" + i.tsk_id + "</li><li>" + i.tsk_name + "</li><li>" + i.tsk_description + "</li></ul>"
+        )
+      })
+
+      removeAllChilds(dataContainer)
+
+      data.forEach(i => {
+        dataContainer.innerHTML += i
+      })
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
+
+removeAllChilds = (el) => {
+  let child = el.lastElementChild
+
+  while (child) {
+    el.removeChild(child)
+    child = el.lastElementChild
+  }
+}
+
+const addItemInList = async (name, description, id) => {
+
+  try {
+    const response = await axios.post('http://localhost:8000/task/add', { name, description, id })
+    if (response.data.success == '1') {
+      getAllTasksAndRenderData(id);
+    }
+
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+
+removeItemInList = () => {
+
+}
+
+add_task.addEventListener('click', () => {
+  const tsk_name = document.getElementById('name').value
+  const tsk_description = document.getElementById('description').value
+
+  addItemInList(tsk_name, tsk_description, getUserId())
+})
