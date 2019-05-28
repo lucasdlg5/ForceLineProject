c4f292fb0132ca247d36e421b2ad1a423bb668d1
Gustavo <gustavo.silva6785@gmail.com>
2019-05-27 23:01:12 -0300

    deploy

--- fabfile.py
+++ fabfile.py
@@ -7,21 +7,8 @@ env.deploy_project_root = '/project/ForceLineProject'
 
 def docker_deployer():
 	with cd('%s' % env.deploy_project_root):
-'''
-		print('stop and deleting docker')
-		sudo("docker stop forceline")
-		sudo("docker rm forceline")
-'''
+		
 		print('getting the changes of github')
 		sudo("git pull")
-'''
-		print("login docker")
-		sudo("docker login -u 'forcelinerobot' -p '789456qwe'")
 
-		print("creating docker")
-		sudo("docker build -t forcelineproject:latest todoapp/ ")
-
-		print("runing docker")
-		sudo("docker run --name forceline -p 8000:80 forcelineproject:latest")
-'''
 		sudo("python3 /todoapp/manage.py runserver")
