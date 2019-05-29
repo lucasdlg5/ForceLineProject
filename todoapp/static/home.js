8f9a675c0a983f8147fc046f8d092b00a290f775
mayaracsferreira <mayara_csf@hotmail.com>
2019-05-29 20:31:15 -0300

    bug fix: input limpo ao inserir novo dado

--- todoapp/static/home.js
+++ todoapp/static/home.js
@@ -1,14 +1,37 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    pegarElemento(){
-      document.getElementById("outer-modal").style.display="block"
-      alert("ooooi")
-    }
-    
-    }
-})
+Vue.use(VueMaterial.default)
 
+        new Vue({
+            el: '#app',
+            data: {              
+                    
+                    nome: "",
+                    desc: "",
+                    teste: [],
+                    count: 0
+                
+            },
+            methods: {
+                pegarElemento() {
+                    document.getElementById("outer-modal").style.display = "block"                    
+                },
+                cancelarElemento(){
+                    document.getElementById("outer-modal").style.display = "none"                    
+                },
+                increment(){
+                    this.counter();
+                    this.teste.push({
+                        'id': this.count,
+                        'nome': this.nome,
+                        'desc': this.desc
+                    })                    
+                },
+                counter(){
+                    this.count ++;
+                },
+                removerRegistro(e){
+                    var index = this.teste.indexOf(e);
+                    this.teste.splice(index,1);
+                }
+                
+            }
+        })
