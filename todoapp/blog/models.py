064e9f8c711765b7a8519da00641a5bf4005ff6f
kevin799 <kevinmikio23@gmail.com>
2019-06-03 21:13:23 -0300

    Atualizacao

--- todoapp/blog/models.py
+++ todoapp/blog/models.py
@@ -17,10 +17,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-class Primeiro_Migrate_SQN(models.Model):
+
+class Primeiro_Migrate_SQN2(models.Model):
     teste_titulo  = models.CharField(max_length=200)
     teste_texto = models.TextField()
     teste_date = models.DateTimeField(
             default=timezone.now)
     teste_date2 = models.DateTimeField(
             blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
